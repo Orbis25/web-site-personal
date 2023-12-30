d56dbce12816ec279a3592f4cd9851d2a0d9fd76
peterramirez18 <peterramirez1812@gmail.com>
2023-12-30 14:40:52 -0400

    add Mouse Event Type

--- src/utils/useMousePosition.tsx
+++ src/utils/useMousePosition.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 const useMousePosition = () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
-  const updateMousePosition = (e) => {
+  const updateMousePosition = (e: MouseEvent) => {
     setMousePosition({
       x: e.clientX + window.scrollX,
       y: e.clientY + window.scrollY,
